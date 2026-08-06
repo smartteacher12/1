862bcab4a824669ad70c3a9d034023e87fca3009
Eyadmano <smartteacher900@gmail.com>
2026-08-06 19:12:16 +0300

    Create tailwind.config.js

---
+++ frontend/frontend/frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#60A5FA',
+        accent: '#2563EB',
+        success: '#10B981',
+        danger: '#EF4444'
+      }
+    }
+  },
+  plugins: []
+};
